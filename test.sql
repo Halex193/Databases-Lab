61b11d96d97f6b5a69a6db39590bec784dc30606
Horațiu Udrea <udrea.horatiu@gmail.com>
2019-11-28 13:56:15 +0200

    More pain

--- test.sql
+++ test.sql
@@ -22,6 +22,9 @@ AS
     DELETE FROM Tests
     DELETE FROM Tables
     DELETE FROM Views
+    DELETE FROM TestRuns
+    DELETE FROM TestRunTables
+    DELETE FROM TestRunViews
 
     INSERT INTO Tables (Name) VALUES ('Posters'), ('Reservations'), ('Companies')
     INSERT INTO Views (Name) VALUES ('PC_prices'), ('ReservationsPerDay'), ('ShowcasedCompanies')
@@ -34,7 +37,7 @@ GO
 
 CREATE OR ALTER PROCEDURE Test (@TestName NVARCHAR(200))
 AS
-    INSERT INTO TestRuns (Description) VALUES (@TestName)
+    INSERT INTO TestRuns (Description, StartAt) VALUES (@TestName, current_timestamp)
     DECLARE @TestRunID INT = @@IDENTITY
 
     DECLARE
@@ -44,31 +47,90 @@ AS
     @StartAt DATETIME,
     @EndAt DATETIME;
 
-DECLARE @TableCursor CURSOR
-FOR SELECT T2.TableID, T2.Name, TT.NoOfRows FROM Tests T JOIN TestTables TT ON T.TestID = TT.TestID JOIN Tables T2 ON TT.TableID = T2.TableID
-    WHERE T.Name = @TestName
-    ORDER BY Position
+    DECLARE TableCursor CURSOR FOR SELECT T2.TableID, T2.Name, TT.NoOfRows FROM Tests T JOIN TestTables TT ON T.TestID = TT.TestID JOIN Tables T2 ON TT.TableID = T2.TableID
+        WHERE T.Name = @TestName
+        ORDER BY Position
 
-OPEN @TableCursor;
+    OPEN TableCursor;
 
-FETCH NEXT FROM @TableCursor INTO
-    @Id,
-    @Name,
-    @NoOfRows;
+    FETCH NEXT FROM TableCursor INTO
+        @Id,
+        @Name,
+        @NoOfRows;
 
-WHILE @@FETCH_STATUS = 0
-    BEGIN
-        SET @StartAt = current_timestamp
-        -- TODO DELETE FROM sys.objects()
-        FETCH NEXT FROM @TableCursor INTO
-            @Id,
-            @Name,
-            @NoOfRows;
-    END;
+    WHILE @@FETCH_STATUS = 0
+        BEGIN
+            SET @StartAt = current_timestamp
+            DECLARE @Statement NVARCHAR(4000)= N'DELETE FROM ' + @Name
+            EXECUTE sp_executesql @Statement
+            EXECUTE PopulateTable @Name, @NoOfRows
+            SET @EndAt = current_timestamp
 
-CLOSE @TableCursor;
-DEALLOCATE @TableCursor;
+            INSERT INTO TestRunTables (TestRunID, TableID, StartAt, EndAt) VALUES (@TestRunID, @Id, @StartAt, @EndAt)
+
+            FETCH NEXT FROM TableCursor INTO
+                @Id,
+                @Name,
+                @NoOfRows;
+        END;
+
+    CLOSE TableCursor;
+    DEALLOCATE TableCursor;
 
     DECLARE @Views TABLE(ViewId INT, Name NVARCHAR(200)) SELECT V.ViewID, V.Name FROM Tests T JOIN TestViews TV ON T.TestID = TV.TestID JOIN Views V ON TV.ViewID = V.ViewID
     WHERE T.Name = @TestName
+
+    UPDATE TestRuns SET EndAt = current_timestamp WHERE TestRunID = @TestRunID
+GO
+
+CREATE OR ALTER PROCEDURE PopulateTable (@TableName NVARCHAR(200), @NoOfRows INT)
+AS
+    PRINT 'Populate Table'
+    DECLARE @Columns TABLE(name NVARCHAR(200), type NVARCHAR(200), is_identity BIT, foreign_table NVARCHAR(200), foreign_column NVARCHAR(200))
+    INSERT INTO @Columns SELECT C.name, TP.name AS type, C.is_identity, T2.name AS foreign_table, C2.name AS foreign_column
+    FROM sys.tables T
+         JOIN sys.schemas S ON T.schema_id = S.schema_id
+         JOIN sys.columns C ON T.object_id = C.object_id
+         JOIN sys.types TP ON C.user_type_id = TP.user_type_id
+        LEFT JOIN sys.foreign_key_columns FC ON C.object_id = FC.parent_object_id AND C.column_id = FC.parent_column_id
+    LEFT JOIN sys.columns C2 ON FC.referenced_column_id = C2.column_id AND FC.referenced_object_id = C2.object_id
+    LEFT JOIN sys.tables T2 ON FC.referenced_object_id = T2.object_id
+    WHERE S.name = 'dbo'
+      AND T.name = @TableName
+
+    DECLARE @NormalColumns NVARCHAR(4000)
+    SELECT @NormalColumns = COALESCE(@NormalColumns + ', ', '') + name
+    FROM @Columns
+    WHERE foreign_column IS NULL AND is_identity = 0
+    ORDER BY type, name
+
+    PRINT @NormalColumns
+
+    DECLARE @ForeignColumns NVARCHAR(4000)
+    SELECT @ForeignColumns = COALESCE(@ForeignColumns + ', ', '') + name
+    FROM @Columns
+    WHERE foreign_column IS NOT NULL AND is_identity = 0
+    ORDER BY type, name
+
+    DECLARE @count INT = 0
+    WHILE @count < @NoOfRows
+    BEGIN
+        DECLARE @IntValues NVARCHAR(4000) = N''
+        SELECT @IntValues = COALESCE(@IntValues + ', ', '') + ROUND(400*RAND(), 0)
+        FROM @Columns
+        WHERE foreign_column IS NULL AND is_identity = 0 AND type = 'int'
+        ORDER BY name
+
+--         PRINT @IntValues
+
+        DECLARE @StringValues VARCHAR(4000)
+        SELECT @StringValues = COALESCE(@StringValues + ', ', '') + 'RANDOM_STRING'
+        FROM @Columns
+        WHERE foreign_column IS NULL AND is_identity = 0 AND type = 'nvarchar'
+        ORDER BY name
+
+        DECLARE @Statement NVARCHAR(4000) = N'INSERT INTO '+@TableName+'('+ @NormalColumns +', '+ @ForeignColumns +') VALUES (`'+@IntValues+'`, `'+@StringValues+'`, `null`)'
+        EXECUTE sp_executesql @Statement
+        SET @count = @count + 1
+    END
 GO
